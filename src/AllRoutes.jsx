37ecae0eae5eeac4df8a891401af4359ca76f748
Rahul <rahulankola13@gmail.com>
2023-02-10 01:33:09 +0530

    Home main bar

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home/Home'
 import Auth from './pages/Auth/Auth'
+import Questions from './pages/Questions/Questions'
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/Auth' element={<Auth/>}></Route>
+        <Route path='/Questions' element={<Questions/>}></Route>
     </Routes>
   )
 }
